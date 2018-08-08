f12a965b18b5bf33190411b7659dc13b909ef2f6
mukki1994 <mukesh.rawat@worksdelight.com>
2018-08-08 18:50:04 +0530

    done

---
+++ app/src/main/java/com/foodpedia/app/activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.foodpedia.app.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import com.foodpedia.app.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,StartupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
---
+++ app/src/main/java/com/foodpedia/app/activity/StartupActivity.java
@@ -0,0 +1,15 @@
+package com.foodpedia.app.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.foodpedia.app.R;
+
+public class StartupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+    }
+}
